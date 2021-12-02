b44388f5b242c52c709ad7a52d60bd3a715dc226
Roland Bergler <roland@bergler.net>
2021-12-02 08:01:59 +0100

    day2

--- src/aoc.rs
+++ src/aoc.rs
@@ -1,3 +1,5 @@
  pub mod file;
  pub(crate) mod day01a;
  pub(crate) mod day01b;
+ pub(crate) mod day02a;
+ pub(crate) mod day02b;
---
+++ src/aoc/day02b.rs
@@ -0,0 +1,85 @@
+use std::path::Path;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use crate::aoc::file;
+
+pub(crate) fn solve() -> i32 {
+    solve_file(&file::input("input02.txt"))
+}
+
+enum Direction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+
+fn solve_file(f: &Path) -> i32 {
+    let numbers = read_input(f);
+    let count = follow(&numbers);
+    count.0*count.1
+}
+
+fn read_input(f: &Path) -> Vec<Direction> {
+    let lines = file::read_lines(f).unwrap();
+    let mut directions = Vec::new();
+    for line in lines {
+        let line = line.unwrap();
+        let direction: Direction = parse(&line).unwrap();
+        directions.push(direction);
+    };
+    directions
+}
+
+
+fn parse(line: &str) -> Result<Direction, &str> {
+    lazy_static! {
+        static ref REF: Regex = Regex::new(r"forward (\d+)").unwrap();
+        static ref REU :Regex= Regex::new(r"up (\d+)").unwrap();
+        static ref RED :Regex= Regex::new(r"down (\d+)").unwrap();
+    }
+    match REF.captures(line) {
+        Some(m) => {
+            Ok(Direction::Forward(m[1].parse().unwrap()))
+        }
+        None => {
+            match REU.captures(line) {
+                Some(m) => Ok(Direction::Up(m[1].parse().unwrap())),
+                None => {
+                    match RED.captures(line) {
+                        Some(m) => Ok(Direction::Down(m[1].parse().unwrap())),
+                        None => Err("syntax")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+fn follow (numbers: &Vec<Direction>) -> (i32,i32, i32) {
+    let mut pos = (0,0, 0);
+    for num in numbers {
+        match num {
+            Direction::Forward(f) => {  pos = (pos.0+f, pos.1+pos.2*f, pos.2)}
+            Direction::Down(d) => {  pos = (pos.0, pos.1, pos.2+d)}
+            Direction::Up(u) => {  pos = (pos.0, pos.1, pos.2-u)}
+        }
+    };
+    pos
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn result() {
+        let result = solve();
+        println!("result : {}", result);
+        assert_eq!(result, 1965970888);
+    }
+}
